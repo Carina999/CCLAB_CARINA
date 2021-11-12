ce4d3c27ceabace06f18d5001aaebd937b7f5d84
Carina <xz3646@nyu.edu>
2021-11-12 14:59:43 +0800

    initial draft

---
+++ jsanddom/js/script.js
@@ -0,0 +1,75 @@
+let i = 0;
+i=i+2;
+// alert(i);
+// alert("20");
+let roundButton =  document.getElementById("roundButton");
+roundButton.addEventListener("click",roundButtonClick);
+function roundButtonClick(){
+  // elem.style.backgroundColor = 'green';
+  let rectElem = document.getElementById('rect');
+  rectElem.style.borderRadius = "50px";
+
+}
+
+
+
+function say(what){
+  // alert(what)
+  // fill(0);
+  // ellipse(mouseX,mouseY,20,20);
+
+// // 111find the element we want to manipulate
+// let elem = document.getElementById('lyrics');
+// // console.log(elem);
+//
+// // 222create a new element to be added
+// let newElem = document.createElement('p');
+//
+// // 2.5change the element's content
+// newElem.innerHTML = what;
+//
+// //3 add the new element to our lyrics
+// elem.appendChild(newElem);
+
+//
+// let elem = document.getElementById('heading');
+// if(what == "better"){
+//   // background-color (in css)
+//   elem.style.backgroundColor = "rgb(255,0,0)";
+// }else if (what == "faster"){
+//   elem.style.backgroundColor = 'green';
+//
+// }
+
+
+
+
+
+
+
+
+// let rect = document.getElementById("rect")
+// let rectElem =document.getElementById("rect")
+//
+// if(what == "better"){
+//   // background-color (in css)
+//   // elem.style.backgroundColor = "rgb(255,0,0)";
+//   rectElem.style.borderRadius = "0px";
+// }else if (what == "faster"){
+//   // elem.style.backgroundColor = 'green';
+//   rectElem.style.borderRadius = "50px";
+// }
+
+
+
+
+
+
+
+
+
+
+
+}
+// fill(0);
+// ellipse(mouseX,mouseY,20,20);
