8aa4346a4c217785d0593753a1b5237b54c26e23
Carina <xz3646@nyu.edu>
2021-11-18 10:51:58 +0800

    interface for artists

--- jsanddom/js/script.js
+++ jsanddom/js/script.js
@@ -1,110 +1,69 @@
-let i = 0;
-i=i+2;
-// alert(i);
-// alert("20");
-let roundButton =  document.getElementById("roundButton");
-roundButton.addEventListener("click",roundButtonClick);
-function roundButtonClick(){
-  // elem.style.backgroundColor = 'green';
-  let rectElem = document.getElementById('rect');
-  rectElem.style.borderRadius = "50px";
-
+alert("Are you ready to be an artist?");
+function NewWindow() {
+  window.open("https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwj7mbf75J_0AhXP0pYKHeFBDeYYABADGgJ0bA&ae=2&ohost=www.google.com&cid=CAASE-RohkY_PBAWCryk8kkrlcu9V5M&sig=AOD64_0OI3dun1OSpRDf1JCUwLQLF88Lag&q&adurl&ved=2ahUKEwik4a_75J_0AhUnk1YBHcHRBbMQ0Qx6BAgGEAE", "_blank", "width=800,height=800" );
 }
 
 
 
-function say(what){
-  // alert(what)
-  // fill(0);
-  // ellipse(mouseX,mouseY,20,20);
-
-// // 111find the element we want to manipulate
-// let elem = document.getElementById('lyrics');
-// // console.log(elem);
-//
-// // 222create a new element to be added
-// let newElem = document.createElement('p');
-//
-// // 2.5change the element's content
-// newElem.innerHTML = what;
-//
-// //3 add the new element to our lyrics
-// elem.appendChild(newElem);
-
-//
-// let elem = document.getElementById('heading');
-// if(what == "better"){
-//   // background-color (in css)
-//   elem.style.backgroundColor = "rgb(255,0,0)";
-// }else if (what == "faster"){
-//   elem.style.backgroundColor = 'green';
-//
-// }
-
-
-
-
-
-
-
-
-// let rect = document.getElementById("rect")
-// let rectElem =document.getElementById("rect")
-//
-// if(what == "better"){
-//   // background-color (in css)
-//   // elem.style.backgroundColor = "rgb(255,0,0)";
-//   rectElem.style.borderRadius = "0px";
-// }else if (what == "faster"){
-//   // elem.style.backgroundColor = 'green';
-//   rectElem.style.borderRadius = "50px";
-// }
-
-
-
-}
-
-let radius = 20;
+let sliderc,slider1,slider2,slider3,sliderr,sliderb;
 
 function setup() {
-  createCanvas(800, 800);
-  //
-  //痕迹
+  let cc = createCanvas(600,600);
+  cc.parent("container");
+// cc.id("container")
+slider1js = document.getElementById("slider1");
+slider2js = document.getElementById("slider2");
+slider3js = document.getElementById("slider3");
+sliderrjs = document.getElementById("sliderr");
+sliderbjs = document.getElementById("sliderb");
+
+
 }
 
 function draw() {
-  background(0, 10);
+  background(0, 20);
+  let an = radians(frameCount*2);
+  let r = map(cos(an), -1, 1, 241, 150);
+  let g = map(sin(an), -1, 1, 211, 183);
+  let b = map(sin(an),-1,1,255,200);
+  let fr = int(sliderrjs.value);
+  let fb = int(sliderbjs.value);
+  let xx = int(slider2js.value);
+  let yy = int(slider3js.value);
+  fill(fr,150,fb);
 
-  let angle = radians(frameCount * 1.3); //改转速
-  let X1 = radius * 1.6 * cos(angle);
-  let Y1 = radius * sin(angle);
-  let X2 = radius * 1.6 * cos(angle + 0.5 * PI);
-  let Y2 = radius * sin(angle + 0.5 * PI);
-  let X3 = radius * 1.6 * cos(angle + PI);
-  let Y3 = radius * sin(angle + PI);
-  let X4 = radius * 1.6 * cos(angle + 1.5 * PI);
-  let Y4 = radius * sin(angle + 1.5 * PI);
+  let mousex = int(slider1js.value);
 
-  let r = map(cos(angle), -1, 1, 241, 115);
-  let g = map(sin(angle), -1, 1, 211, 183);
-  let b = map(cos(angle), -1, 1, 255, 230);
-
-  push();
   translate(width / 2, height / 2);
-  rotate(radians(20));
-  stroke(r, g, b, 200);
-  // line(0, 0, X, Y);
-  fill(r, g, b);
-  noStroke();
-  ellipse(X1, Y1, 10);
-  ellipse(X2, Y2, 10);
-  ellipse(X3, Y3, 10);
-  ellipse(X4, Y4, 10);
-  pop();
-
-  if (frameCount > 800) {
-    radius = radius;
-  } else {
-    radius = radius + 0.05; //改变大的速度
+  //middle
+  for (let angle = 0; angle < 360; angle = angle + max(mousex, 4)) {
+    push();
+    rotate(radians(angle));
+    noStroke();
+    ellipse(0, 142, 8, 8);
+    stroke(r, g, b, 100);
+    line(0, xx, xx, yy);
+    line(0, 100, -100, 100);
+    line(0, 100, 100, 100);
+    pop();
+  }
+  //outer
+  for (let angle = 0; angle < 360; angle = angle + max(mousex, 4)) {
+    push();
+    rotate(radians(angle));
+    ellipse(0, 200, 12, 3);
+    pop();
+  }
+  //inner
+  for (let angle = 0; angle < 360; angle = angle + max(mousex, 6)) {
+    push();
+    rotate(radians(angle));
+    stroke(r, g, b, 100);
+    line(0, 0, 0, 100);
+    line(0, 20, 63, 21.5);
+    line(0, 0, 30, 30);
+    noStroke();
+    ellipse(0, 100, 7, 7);
+    pop();
   }
 }
